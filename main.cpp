83accadee3d7a5740383b412d84da7eb2c5700a4
Aleksei Nikiforov <a.nikiforov@negative.ee>
2018-09-04 19:33:03 +0300

    Clear code and second variant

--- main.cpp
+++ main.cpp
@@ -19,7 +19,6 @@
 
 #include "iostream"
 #include "string"
-#include <cmath>
 
 #define TOLERANTS 0.05
 using namespace std;
@@ -40,8 +39,6 @@ int main()
 
     cin >> realWeight;
 
-    const float realWeightForPrint = realWeight; // Приравнял к константе, т.к. при введени строки отличной от "w", "W", "m", "M" (этой версии в коде нет, с исполшьзованием putchar tolower - не заработала, оставил только рабочий код, инициализацию переменной не стал менять), к примеру, Man - переменная меняет значение
-
     float idealWeight;
     char sex; // Объявляем переменную для определения юзера по половому признаку
 
@@ -58,24 +55,86 @@ int main()
         cout << "\nYou are man" << endl;
     } else {
         cout << "\nYou are not human" << endl;
-        printf("Bye!\n");
-        terminate(); // Завершаем процесс, если были указаны не w или n (W,N)
+        cout << "Bye!\n";
+        return 0;
     }
     float minWeight = idealWeight * (1.0 - TOLERANTS);
     float maxWeight = idealWeight * (1.0 + TOLERANTS);
 
-    cout << "\nYour real weight is " << realWeightForPrint <<" kg and your ideal weight shall be between (" << minWeight << " - "<< maxWeight <<") kg\n";
+    cout << "\nYour real weight is " << realWeight <<" kg and your ideal weight shall be between (" << minWeight << " - "<< maxWeight <<") kg\n";
 
-    if (realWeightForPrint < minWeight) {
+    if (realWeight < minWeight) {
         cout << "\nOverview: You under ideal weight\n" << endl;
     }
-    if ( (realWeightForPrint > minWeight) && (realWeightForPrint < maxWeight )){
+    if ( (realWeight > minWeight) && (realWeight < maxWeight )){
          cout << "\nOverview: You have ideal weight\n" << endl;
     }
-    if (realWeightForPrint > maxWeight) {
+    if (realWeight > maxWeight) {
          cout << "\nOverview: You above ideal weight\n" << endl;
     }
 
     return 0;
 
 }
+
+/*
+ * Второй вариант
+ *
+// main - главная функция
+int main()
+{
+    // вывод на консоль
+    cout << "Hello user \n";
+    cout << "What is your heigth in cm? " << endl;
+
+    unsigned short int height = 0;
+
+    cin >> height;
+    cout << "What is your weight in kg? " << endl;
+
+    float realWeight;
+
+    cin >> realWeight;
+
+    float idealWeight;
+    string sex; // Объявляем переменную для определения юзера по половому признаку
+
+    cout << "What is your sex? Press button 'w' if you are woman or 'm' if you are man" << endl;
+    cin >> sex;
+    //cout << "Sex is : " << sex << " first char is ["<<sex[0]<<"] \n";
+
+    char sexFirstLetter = putchar (tolower(sex[0]));
+
+    if (sexFirstLetter == 'w') {
+        idealWeight = height * height * 0.00222;
+        cout << "\nYou are woman" << endl;
+    }
+
+    else if (sexFirstLetter == 'm')  {
+        idealWeight = height * height * 0.00227;
+        cout << "\nYou are man" << endl;
+    } else {
+        cout << "\nYou are not human" << endl;
+        cout << "Bye!\n";
+        return 0;
+    }
+    float minWeight = idealWeight * (1.0 - TOLERANTS);
+    float maxWeight = idealWeight * (1.0 + TOLERANTS);
+
+    cout << "\nYour real weight is " << realWeight <<" kg and your ideal weight shall be between (" << minWeight << " - "<< maxWeight <<") kg\n";
+
+    if (realWeight < minWeight) {
+        cout << "\nOverview: You under ideal weight\n" << endl;
+    }
+    if ( (realWeight > minWeight) && (realWeight < maxWeight )){
+         cout << "\nOverview: You have ideal weight\n" << endl;
+    }
+    if (realWeight > maxWeight) {
+         cout << "\nOverview: You above ideal weight\n" << endl;
+    }
+
+    return 0;
+
+}
+
+*/
